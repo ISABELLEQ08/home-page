22e299d659bfd3479aebb78a6b515970730bbfab
Isabelle Quach <belleq358@gmail.com>
2023-07-19 13:26:36 -0700

    Initial Commit

---
+++ home page/home_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  home_pageApp.swift
+//  home page
+//
+//  Created by scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct home_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
